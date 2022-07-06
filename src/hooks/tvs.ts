89fabb374fb7a3bd30ae625033c54c802b0e5baf
Gyujae <wjdrbwo1206@naver.com>
2022-07-06 16:46:51 +0900

    feat: tv service and hook

---
+++ src/hooks/tvs.ts
@@ -0,0 +1,65 @@
+import TvsService from 'services/tvs.service'
+
+import { ITVCredits, ITVDetail, ITVGenres, ITVResult, TvCategory } from 'types/tv.d'
+import { useInfiniteQuery, useQuery } from 'react-query'
+
+const services = new TvsService()
+
+export const useTvs = (category: TvCategory) => {
+  return useQuery<ITVResult, Error>(['tvs', category], () => services.getTvs(category))
+}
+
+export const useTv = (id: string) => {
+  return useQuery<ITVDetail, Error>(['tv', id], () => services.getTV(id))
+}
+
+export const useTVRecommendations = (id: string) => {
+  return useQuery<ITVResult, Error>(['tv', id, 'recommendations'], () => services.getRecommendations(id))
+}
+
+export const useTVCredits = (id: string) => {
+  return useQuery<ITVCredits, Error>(['tv', id, 'credits'], () => services.getCredits(id))
+}
+
+export const useTVSimilar = (id: string) => {
+  return useQuery<ITVResult, Error>(['tv', id, 'similar'], () => services.getSimilar(id))
+}
+
+export const useTVGenres = () => {
+  return useQuery<ITVGenres, Error>(['tv', 'genres'], () => services.getGenres())
+}
+
+export const useInfiniteTVs = (category: TvCategory) => {
+  return useInfiniteQuery<ITVResult, Error>(
+    ['tv', category],
+    ({ pageParam = 1 }) => services.getPageTVs({ category, pageParam }),
+    {
+      getNextPageParam: (lastPage: ITVResult) => {
+        if (lastPage.page < lastPage.total_pages) return lastPage.page + 1
+        return undefined
+      },
+      refetchOnWindowFocus: false,
+      refetchOnMount: true,
+      refetchOnReconnect: true,
+      retry: 1,
+    }
+  )
+}
+
+export const useInfiniteSearchTVs = (query: string) => {
+  return useInfiniteQuery<ITVResult, Error>(
+    ['tvs', 'search', query],
+    ({ pageParam = 1 }) => services.getSearch({ query, pageParam }),
+    {
+      getNextPageParam: (lastPage: ITVResult) => {
+        if (lastPage.page < lastPage.total_pages) return lastPage.page + 1
+        return undefined
+      },
+      enabled: !!query,
+      refetchOnWindowFocus: false,
+      refetchOnMount: true,
+      refetchOnReconnect: true,
+      retry: 1,
+    }
+  )
+}
---
+++ src/services/tvs.service.ts
@@ -0,0 +1,101 @@
+import axios from 'axios'
+
+import { ITVCredits, ITVDetail, ITVGenres, ITVResult, TvCategory } from 'types/tv.d'
+
+class TvsService {
+  private apiURL: string
+  private REACT_APP_MOVIE_API_KEY: string
+  constructor() {
+    this.apiURL = 'https://api.themoviedb.org/3/'
+    this.REACT_APP_MOVIE_API_KEY = process.env.REACT_APP_MOVIE_API_KEY as string
+  }
+  private makeApiCall = async <T>(apiPath: string): Promise<T> => {
+    const response = await axios.get(`${this.apiURL}${apiPath}`, {
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+    if (!response.data) {
+      throw new Error(`Error from api call ${apiPath}: status=${response.status} ${response.statusText}`)
+    }
+    return response.data
+  }
+
+  public getTvs = async (category: TvCategory): Promise<ITVResult> => {
+    const response = await this.makeApiCall<ITVResult>(`tv/${category}?api_key=${this.REACT_APP_MOVIE_API_KEY}`)
+    if (!response.results) {
+      throw new Error('TVs not found')
+    }
+    return response
+  }
+
+  public getPageTVs = async ({
+    category,
+    pageParam = 1,
+  }: {
+    category: TvCategory
+    pageParam?: number
+  }): Promise<ITVResult> => {
+    const response = await this.makeApiCall<ITVResult>(
+      `tv/${category}?api_key=${this.REACT_APP_MOVIE_API_KEY}&page=${pageParam}`
+    )
+    if (!response.results) {
+      throw new Error('TV Shows not found')
+    }
+    return response
+  }
+
+  public getTV = async (id: string) => {
+    const response = await this.makeApiCall<ITVDetail>(`tv/${id}?api_key=${this.REACT_APP_MOVIE_API_KEY}`)
+    if (!response.id) {
+      throw new Error('TV not found')
+    }
+    return response
+  }
+
+  public getRecommendations = async (id: string) => {
+    const response = await this.makeApiCall<ITVResult>(
+      `tv/${id}/recommendations?api_key=${this.REACT_APP_MOVIE_API_KEY}`
+    )
+    if (!response.results) {
+      throw new Error('Movies not found')
+    }
+    return response
+  }
+
+  public getCredits = async (id: string) => {
+    const response = await this.makeApiCall<ITVCredits>(`tv/${id}/credits?api_key=${this.REACT_APP_MOVIE_API_KEY}`)
+    if (!response.id) {
+      throw new Error('Movie not found')
+    }
+    return response
+  }
+
+  public getSimilar = async (id: string) => {
+    const response = await this.makeApiCall<ITVResult>(`tv/${id}/similar?api_key=${this.REACT_APP_MOVIE_API_KEY}`)
+    if (!response.results) {
+      throw new Error('Movies not found')
+    }
+    return response
+  }
+
+  public getGenres = async () => {
+    const response = await this.makeApiCall<ITVGenres>(`genre/movie/list?api_key=${this.REACT_APP_MOVIE_API_KEY}`)
+    if (!response.genres) {
+      throw new Error('Genres not found')
+    }
+    return response
+  }
+
+  public getSearch = async ({ query, pageParam = 1 }: { query: string; pageParam?: number }) => {
+    const response = await this.makeApiCall<ITVResult>(
+      `search/tv?api_key=${this.REACT_APP_MOVIE_API_KEY}&query=${query}&page=${pageParam}`
+    )
+    if (!response.results) {
+      throw new Error('Genres not found')
+    }
+    return response
+  }
+}
+
+export default TvsService
