06eb33f1ba801b3427a1d21e1c7aac0a3847089c
Gyujae <wjdrbwo1206@naver.com>
2022-07-07 16:08:59 +0900

    feat: tv pages Categories component

--- src/routes/Discovery/Header/TVs/index.tsx
+++ src/routes/Discovery/Header/TVs/index.tsx
@@ -1,11 +1,24 @@
+import { AnimatePresence } from 'framer-motion'
+import { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { cx } from 'styles'
+import Categories from './Categories'
 import styles from './tvs.module.scss'
 
 const TVs = () => {
+  const [openCategories, setOpenCategories] = useState<boolean>(false)
   const [searchParams] = useSearchParams()
   const mediaType = searchParams.get('mediaType') || 'movie'
-  return <li className={cx(styles.item, { [styles.current]: mediaType === 'tv' })}>TV Shows</li>
+  const handleMouseEnter = () => setOpenCategories(true)
+  const handleMouseLeave = () => setOpenCategories(false)
+  return (
+    <li className={cx(styles.item, { [styles.current]: mediaType === 'tv' })} onMouseEnter={handleMouseEnter}>
+      TV Shows
+      <AnimatePresence>
+        <Categories inView={openCategories} handleMouseLeave={handleMouseLeave} />
+      </AnimatePresence>
+    </li>
+  )
 }
 
 export default TVs
